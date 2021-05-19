6d8c852f61fef326f5eead7fe78b9ece5b56e25d
crybabytearsfall@gmail.com <crybabytearsfall@gmail.com>
2021-05-19 13:04:56 +0300

    Сделали вывод фигур по нажатию на панель

--- gugu/Form2.Designer.cs
+++ gugu/Form2.Designer.cs
@@ -30,20 +30,104 @@ namespace gugu
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BtnRec = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 38);
+            this.textBox1.Location = new System.Drawing.Point(12, 44);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(550, 20);
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
             this.textBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(12, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 288);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отрезок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Окружность";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnRec
+            // 
+            this.BtnRec.Location = new System.Drawing.Point(397, 109);
+            this.BtnRec.Name = "BtnRec";
+            this.BtnRec.Size = new System.Drawing.Size(102, 23);
+            this.BtnRec.TabIndex = 4;
+            this.BtnRec.Text = "Прямоугольник";
+            this.BtnRec.UseVisualStyleBackColor = true;
+            this.BtnRec.Click += new System.EventHandler(this.Rec);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(397, 148);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Треугольник";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Задайте параметры";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(456, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Произвольная фигура";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(620, 405);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BtnRec);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -56,5 +140,12 @@ namespace gugu
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BtnRec;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- gugu/Form2.cs
+++ gugu/Form2.cs
@@ -12,12 +12,90 @@ namespace gugu
 {
     public partial class Form2 : Form
     {
+        public enum TypeFigure
+        {
+            Null,
+            Rectangle,
+            Circle,
+            Line,
+            Triangle
+        }
+
+        TypeFigure fig = TypeFigure.Null;
+        Graphics paper;
+        Pen pen;
         public Form2()
         {
             InitializeComponent();
         }
-
         private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fig = TypeFigure.Circle;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Rec(object sender, EventArgs e)
+        {
+            fig = TypeFigure.Rectangle;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fig = TypeFigure.Line;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fig = TypeFigure.Triangle;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (fig != TypeFigure.Null)
+            {
+                Draw(e);
+            }
+        }
+
+        private void Draw(MouseEventArgs data)
+        {
+            paper = panel1.CreateGraphics();
+            pen = new Pen(Color.Pink, 5);
+
+            switch (fig)
+            {
+                case TypeFigure.Circle:
+                    paper.DrawEllipse(pen, data.X, data.Y, 50, 50);
+                    break;
+                case TypeFigure.Rectangle:
+                    paper.DrawRectangle(pen, data.X, data.Y, 100, 100);
+                    break;
+                case TypeFigure.Line:
+                    paper.DrawLine(pen, data.X, data.Y, data.X + 100, data.Y + 100);
+                    break;
+                case TypeFigure.Triangle:
+                    paper.DrawLine(pen, data.X, data.Y, data.X + 51, data.Y + 51);
+                    paper.DrawLine(pen, data.X, data.Y, data.X - 51, data.Y + 51);
+                    paper.DrawLine(pen, data.X - 51, data.Y + 51, data.X + 51, data.Y + 51);
+                    break;
+
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
